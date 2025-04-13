932edd54e0101c67de3b3465507412162243910c
Moaz El Gandy <zezogameing1234@gmail.com>
2025-04-13 12:47:10 +0200

    Remove Contact component and ContactBanner from About page; update metadata formatting

--- src/app/[locale]/about/page.js
+++ src/app/[locale]/about/page.js
@@ -1,18 +1,18 @@
 import React from "react";
 import WhyChooseUs from "@/app/components/About/WhyChooseUs";
 import dynamic from "next/dynamic";
-const ContactBanner = dynamic(() => import("@/app/components/About/ConnectBanner"));
+
 const HowItWorks = dynamic(() => import("@/app/components/About/HowItWork"));
 
 export const metadata = {
-  title: '4P - About Us',
-}
+  title: "4P - About Us",
+};
 
 const page = () => {
   return (
     <div>
       <WhyChooseUs />
-      <ContactBanner />
+
       <HowItWorks />
     </div>
   );
--- src/app/[locale]/contact/page.js
+++ src/app/[locale]/contact/page.js
@@ -1,21 +1,13 @@
-import React from 'react'
-import Contact from '@/app/components/contact/Contact'
-import dynamic from 'next/dynamic';
-const About = dynamic(() => import('@/app/components/About/About'));
-const ContactBanner = dynamic(() => import('@/app/components/About/ConnectBanner'));
+"use client";
+
+import { notFound } from "next/navigation";
 
 export const metadata = {
-  title: '4P - Contact Us',
-}
+  title: "4P - Contact Us",
+};
 
-const page = async() => {
-  return (
-    <>
-       <Contact/>
-       <ContactBanner/>
-        <About/>
-    </>
-  )
-}
+const page = async () => {
+  return notFound();
+};
 
-export default page
+export default page;
