dc98142478bad5f32ccdfacb87d50f677a840469
Moaz El Gandy <zezogameing1234@gmail.com>
2025-04-13 11:29:00 +0200

    COMMIT

---
+++ localUrl.js
@@ -0,0 +1,6 @@
+// export const localApi = 'http://192.168.1.43/4P/public'
+// export const localImage = 'http://192.168.1.43/4P/public'
+// export const baseUrl = "http://localhost:3000"
+export const localApi = 'https://admin.4papp.com'
+export const localImage = 'https://admin.4papp.com'
+export const baseUrl = "https://admin.4papp.com"
